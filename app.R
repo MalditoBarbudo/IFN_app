5dc4658159c96a0fe22acb7ac91077e1340d5c8e
MalditoBarbudo <victorgrandagarcia@gmail.com>
2018-05-24 17:58:48 +0200

    basic functionality added

--- app.R
+++ app.R
@@ -1,48 +1,151 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(leaflet)
+library(sp)
+library(rgdal)
+library(tidyverse)
+library(dbplyr)
+library(RPostgreSQL)
+library(viridis)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+## SOURCES ####
+source('global.R', local = TRUE)
+
+## VARS ####
+vars <- c(
+  'Precipitació' = 'precipitacioanual',
+  'Temperatura' = 'temperaturamitjanaanual'
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+## UI ####
+ui <- navbarPage(
+  # opts
+  title = "Eines d'anàlisi IFN",
+  id = 'nav',
+  collapsible = TRUE,
+  
+  # contents
+  tabPanel(
+    "Mapa interactiu",
+    
+    div(
+      class = "outer",
+      tags$head(
+        # custom css
+        includeCSS('resources/ifn.css')
+        # custom scripts
+        # includeScript("resources/gomap.js")
+      ),
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+      # map output
+      leafletOutput(
+        'ifn_map', width = '100%', height = '100%'
+      ),
+      
+      # overlay panel with controls and selected parcels
+      absolutePanel(
+        id = 'controls', class = 'panel panel-default', fixed = TRUE,
+        draggable = TRUE, top = 320, left = 'auto', right = 20, bottom = 'auto',
+        width = 330, height = 'auto',
+        
+        h2("Pinta y Colorea"),
+        
+        selectInput('color', 'Color', vars),
+        selectInput('size', 'Mida', vars)
+      ),
+      
+      tags$div(
+        id = 'cite',
+        "Dades compilats pel CREAF & CTFC basats en l'IFN"
+      )
+    )
+  )
+)
+
+## SERVER ####
+server <- function(input, output, session) {
+  
+  # interactive map ####
+  output$ifn_map <- renderLeaflet({
+    
+    leaflet() %>%
+      # addProviderTiles(providers$Hydda.Base, group = 'Base') %>%
+      setView(1.519410, 41.720509, zoom = 8) %>%
+      addPolygons(
+        data = polygons_municipis, group = 'Municipis',
+        weight = 1, smoothFactor = 0.5,
+        opacity = 1.0, fill = FALSE,
+        label = ~NOMMUNI,
+        # fillColor = ~colorQuantile("YlOrRd", ALAND)(ALAND),
+        highlightOptions = highlightOptions(color = "#CF000F", weight = 2,
+                                            bringToFront = TRUE,
+                                            fill = FALSE)
+      ) %>%
+      addPolygons(
+        data = polygons_comarques, group = 'Comarques',
+        weight = 1, smoothFactor = 0.5,
+        opacity = 1.0, fill = FALSE,
+        label = ~NOMCOMAR,
+        # fillColor = ~colorQuantile("YlOrRd", ALAND)(ALAND),
+        highlightOptions = highlightOptions(color = "#CF000F", weight = 2,
+                                            bringToFront = TRUE,
+                                            fill = FALSE)
+      ) %>%
+      addPolygons(
+        data = polygons_vegueries, group = 'Vegueries',
+        weight = 1, smoothFactor = 0.5,
+        opacity = 1.0, fill = FALSE,
+        label = ~NOMVEGUE,
+        # fillColor = ~colorQuantile("YlOrRd", ALAND)(ALAND),
+        highlightOptions = highlightOptions(color = "#CF000F", weight = 2,
+                                            bringToFront = TRUE,
+                                            fill = FALSE)
+      ) %>%
+      addPolygons(
+        data = polygons_provincies, group = 'Provincies',
+        weight = 1, smoothFactor = 0.5,
+        opacity = 1.0, fill = FALSE,
+        label = ~NOMPROV,
+        # fillColor = ~colorQuantile("YlOrRd", ALAND)(ALAND),
+        highlightOptions = highlightOptions(color = "#CF000F", weight = 2,
+                                            bringToFront = TRUE,
+                                            fill = FALSE)
+      ) %>%
+      addLayersControl(
+        baseGroups = c('Provincies', 'Vegueries',
+                       'Comarques', 'Municipis'),
+        options = layersControlOptions(collapsed = FALSE)
+      )
+    
+  })
+  
+  # observer to maintain the color of polygons
+  observe({
+    color_var <- input$color
+    size_var <- input$size
+    
+    # color palette
+    color_vector <- data_parcelas %>%
+      pull(color_var)
+    pal <- colorBin('viridis', color_vector, 9)
+    
+    # size palette
+    # size_vector <- data_parcelas[[size_var]] / max(data_parcelas[[size_var]]) * 3000
+    
+    
+    # update map
+    leafletProxy('ifn_map', data = data_parcelas) %>%
+      addCircles(
+        group = 'Parcelas', lng = ~ long, lat = ~ lat,
+        layerId = ~idparcela, stroke = FALSE, fillOpacity = 0.4,
+        fillColor = pal(color_vector), radius = 500
+      ) %>%
+      addLegend(
+        position = 'bottomright', pal = pal, values = color_vector,
+        title = color_var, layerId = 'color_legend'
+      )
+    
+  })
+  
 }
 
 # Run the application 
---
+++ global.R
@@ -0,0 +1,42 @@
+# Global script
+
+# data for polygons (municipis, comarques, vegueries i provincies)
+polygons_municipis <- readOGR('shapefiles', 'bm5mv20sh0tpm1_20180101_0') %>%
+  spTransform(CRS("+proj=longlat +datum=WGS84"))
+
+polygons_comarques <- readOGR('shapefiles', 'bm5mv20sh0tpc1_20180101_0') %>%
+  spTransform(CRS("+proj=longlat +datum=WGS84"))
+
+polygons_vegueries <- readOGR('shapefiles', 'bm5mv20sh0tpv1_20180101_0') %>%
+  spTransform(CRS("+proj=longlat +datum=WGS84"))
+
+polygons_provincies <- readOGR('shapefiles', 'bm5mv20sh0tpp1_20180101_0') %>%
+  spTransform(CRS("+proj=longlat +datum=WGS84"))
+
+
+# data connection
+oracle_ifn <- DBI::dbConnect(
+  RPostgreSQL::PostgreSQL(),
+  user = 'ifn',
+  password = rstudioapi::askForPassword('Password for ifn'),
+  dbname = 'oracle_ifn'
+)
+
+data_parcelas <- tbl(oracle_ifn, 'parcelaifn2_clima') %>%
+  select(idparcela, precipitacioanual, temperaturamitjanaanual) %>%
+  inner_join(tbl(oracle_ifn, 'parcelaifn2_sig'), by = 'idparcela') %>%
+  collect()
+
+coordinates_parcelas <- data_parcelas[,c('idparcela', 'utm_x', 'utm_y')]
+coordinates(coordinates_parcelas) <- ~utm_x+utm_y
+proj4string(coordinates_parcelas) <- CRS("+init=epsg:25831")
+
+coordinates_par_transf <- spTransform(
+  coordinates_parcelas, CRS("+proj=longlat +datum=WGS84")
+)
+
+data_parcelas <- data_parcelas %>%
+  mutate(long = coordinates_par_transf@coords[,1],
+         lat = coordinates_par_transf@coords[,2])
+
+
